6577db405bb871c6868870022c5c2e579345c016
steveviko <stevenviko@gmail.com>
2018-11-15 20:49:04 +0300

    Add create user endpoints

--- app/views.py
+++ app/views.py
@@ -38,3 +38,26 @@ def create_order():
             parcel =order_obj.add_orders(order)          
             return jsonify({"ParcelList":parcel}), 201
         
+@app.route('/api/v1/users', methods=['POST'])
+def create_user():
+    if request.method == 'POST':
+        #data from the user  
+        result = request.data
+        data=json.loads(result)
+        if not data:
+            return jsonify({'error': 'unsupported Request'}), 400
+        elif 'username' not in data:
+            return jsonify({'error': 'username is requred'}), 400
+        elif 'password' not in data:
+            return jsonify({'error': 'password is required'}), 400
+        username=data["username"]
+        password=data['password']          
+
+        if username=="" or password=="":
+            return jsonify({"Error": "No user Found"}), 400
+            
+        else:
+            user_obj.register_user(username, password)
+            return jsonify({'Message': 'New user registered successfully' }), 201
+            
+           
