8d107aab16d5b53d003c1220fa214085f6eb6b23
steveviko <stevenviko@gmail.com>
2018-11-19 14:58:54 +0300

    Add user_register function

---
+++ app/user_actions.py
@@ -0,0 +1,24 @@
+from app.user import User
+from app.dboperations import DbOperations
+from app.parcels import Parcels
+from werkzeug.security import generate_password_hash, check_password_hash
+
+userObject = User()
+parcelObject = Parcels()
+
+
+
+class UserActions:
+    def __init__(self):
+        self.db_Object = DbOperations()
+        
+
+    def user_register(self, email, password):
+        userObject.email = email
+        userObject.password = password
+        hash_password = generate_password_hash(
+            userObject.password, method='sha256')
+        self.db_Object.register_user(
+            userObject.email, userObject.password, hash_password
+        )
+        return {'message': 'user sucessfully created'}
