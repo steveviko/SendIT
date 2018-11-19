c205e8e05a84c9026b7cf51b63108cf96095e848
steveviko <stevenviko@gmail.com>
2018-11-19 14:37:07 +0300

    Add register_user method

--- app/dboperations.py
+++ app/dboperations.py
@@ -7,3 +7,10 @@ class DbOperations:
     def __init__(self):
         self.connect=Database()
         
+
+    def register_user(self, username,email, hash_password):
+        """ insert a new user into the users table """
+
+        sql = """INSERT INTO users(username,email, hash_password)
+                VALUES(%s,%s, %s);"""
+        return  self.connect.conn(sql, username, email, hash_password)
