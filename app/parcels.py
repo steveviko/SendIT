52de930b6a2d415c268083bf3024a98d84b13961
steveviko <stevenviko@gmail.com>
2018-11-19 10:17:03 +0300

    create parcel object

---
+++ app/parcels.py
@@ -0,0 +1,8 @@
+class Parcels:
+
+    def __init__(self):
+        self.item= None
+        self.description=None
+        self.destination = None
+        self.current_location = None
+        self.status="pending"
