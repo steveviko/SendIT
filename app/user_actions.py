73815c5e21388d5546ad98e81741ec0477ac8150
steveviko <stevenviko@gmail.com>
2018-11-20 10:12:54 +0300

    Add user_actions.py file

---
+++ app/user_actions.py
@@ -0,0 +1,25 @@
+from app.views import app
+from .user import User
+from .dboperations import DbOperations
+from .parcels import Parcels
+from werkzeug.security import generate_password_hash, check_password_hash
+
+userObject = User()
+parcelObject = Parcels()
+
+class UserActions:
+    def __init__(self):
+        self.db_Object = DbOperations()
+        
+
+    def user_register(self,email, password):
+        
+        userObject.email = email
+        userObject.password = password
+        hash_password = generate_password_hash(
+            userObject.password, method='sha256')
+      
+        self.db_Object.register_user(
+            userObject.email, userObject.password, hash_password
+        )
+        return {'message': 'user sucessfully created'}
