551eedb44490f00f33ba2dcd03ce30b1c4aff7a2
steveviko <stevenviko@gmail.com>
2018-11-20 14:06:29 +0300

    Add  add_user method

--- app/dboperations.py
+++ app/dboperations.py
@@ -11,4 +11,10 @@ class DbOperations:
     def __init__(self):
         self.connect=Database()
     
-    
+    def add_user(self,account):
+        """ insert a new user into the users table """
+        command = "INSERT INTO users(username, email,hash_password, role) VALUES(\
+                '{}',  '{}', '{}', 'user')".format(account["username"],  account["email"]\
+                , generate_password_hash(account["password"]))
+        cursor.execute(command)
+        return account
