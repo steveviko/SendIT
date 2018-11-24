a33fccbac49e1062f0ddbc3e70a8009173068879
steveviko <stevenviko@gmail.com>
2018-11-24 18:14:00 +0300

    Update config file

--- config.py
+++ config.py
@@ -8,15 +8,9 @@ class Config:
 class DevelopmentConfig(Config):
     DEVELOPMENT = True    
     DEBUG = True
-    DB_NAME = "test_sendit"
-    DB_USER = "postgres"
-    DB_PASS = "password"
-    DB_HOST = "localhost"
+    
 
 class TestConfig(Config):   
     TESTING = True
    
-    DB_NAME = "test_sendit"
-    DB_USER = "postgres"
-    DB_PASS = "password"
-    DB_HOST = "localhost"
+    
