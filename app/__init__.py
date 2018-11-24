d2db89026913cd0cc2879bfcf0d31d5b0b10f739
steveviko <stevenviko@gmail.com>
2018-11-24 18:17:20 +0300

    Update create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
-
+from config import DevelopmentConfig
+from config import TestConfig
 
 def create_app():    
     app = Flask(__name__, instance_relative_config=True)
-         
+    app.config.from_object(DevelopmentConfig) 
     return app
 
 
